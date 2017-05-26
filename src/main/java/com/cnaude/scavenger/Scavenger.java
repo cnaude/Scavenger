3b61471fb02e3a5ec9e32274f56f73272c597eba
cnaude <cnaude@zebra>
2017-05-25 20:46:46 -0700

    Remove ProtocolLib-API dependency from pom

--- src/main/java/com/cnaude/scavenger/Scavenger.java
+++ src/main/java/com/cnaude/scavenger/Scavenger.java
@@ -485,6 +485,6 @@ public class Scavenger extends JavaPlugin {
     protected boolean isMc19or11x() {
         return (getServer().getVersion().contains("MC: 1.9")
                 || getServer().getVersion().contains("MC: 1.1"));
-}
+    }
 
 }
